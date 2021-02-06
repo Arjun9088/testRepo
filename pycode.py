21163d7ffbf3f040b7dea1bafdd87ad7f9e9c741
Arjun <barjun2009@gmail.com>
2021-02-06 20:09:40 +0530

    Add multiply and divide capability

--- pycode.py
+++ pycode.py
@@ -4,5 +4,13 @@ def add(a, b):
 def sub(a, b):
     return a - b
 
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
 print(add(10,12))
 print(sub(10,12))
+print(mul(10,12))
+print(div(10,12))
